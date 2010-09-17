f220494efdb6c9ff8078742561653117438c0c6b
Magnus Woldrich <trapd00r@trapd00r.se>
2010-09-17 22:42:42 +0200

    added some crap

--- dzen.sh
+++ dzen.sh
@@ -2,11 +2,12 @@
 
 FONT="-windows-montecarlo-medium-r-normal--11-110-72-72-c-60-microsoft-cp1252"
 while true; do 
+  printf "^fg(#484848)  "
+  date +%H:%M
   perl /home/scp1/devel/dzen-scripts/pdzen.pl
-  uptime|awk {'print "^fg(#f80412)"$10"^fg(#f83c04)"$11"^fg(#f86b04)"$12'}|perl -pe 's/,/ ^fg()| /g'
   #perl ./pdzen.pl
-  sleep 3
-done |dzen2 -e 'foo' -ta r -x 0 -y 0 -w 1366  -fg '#ffffff' -bg '#030303' \
+  sleep 2
+done |dzen2 -e 'foo' -ta r -x 0 -y 0 -w 1366  -fg '#ffffff' -bg '#0e0e0e' \
            -fn $FONT -h 15 -u -l 2 -h 17  
 
 # vim: tw=99999:
--- pdzen.pl
+++ pdzen.pl
@@ -3,7 +3,7 @@ use strict;
 
 my $circle  = "^co(2)";
 my $square  = "^r(2x2)";
-my $i_music = "/home/scp1/devel/dzen-scripts/bitmaps/music.xbm";
+my $i_music = "/home/scp1/devel/dzen-scripts/bitmaps/musicS.xbm";
 
 sub date {
   my @date  = localtime(time);
@@ -11,30 +11,61 @@ sub date {
   return $fdate;
 }
 
+#TODO
+
+sub uptime {
+  chomp(my $up = `uptime`);
+  my ($_1,$_5,$_15) = $up =~ /average: (.+),(.+),(.+)$/;
+  $_1 =~ s/\s+//;
+  $_5 =~ s/\s+//;
+  $_15 =~ s/\s+//;
+
+  $_1 = load($_1);
+  $_5 = load($_5);
+  $_15 = load($_15);
+
+  return sprintf("$_1, $_5, $_15");
+
+}
+
+sub load {
+  my $load = shift;
+  if($load > 0.30 and $load < 0.50) {
+    $load = "^fg(#03ab4a)$load^fg()";
+  }
+  elsif($load > 0.75 and $load < 1.00) {
+    $load = "^fg(#f2b30e)$load^fg()";
+  }
+  elsif($load > 1.00) {
+    $load = "^fg(#ff0000)$load^fg()";
+  }
+  else {
+    $load = "^fg(#15f20e)$load^fg()";
+  }
+  return($load);
+}
+
+
+
 sub mpd {
   use Audio::MPD;
   my $mpd = Audio::MPD->new(
     host => '192.168.1.101',
   );
+  my $status = "/home/scp1/devel/dzen-scripts/bitmaps/" . $mpd->status->state . '.xbm';
   my $artist = $mpd->current->artist // 'undef';
   my $album  = $mpd->current->album  // 'undef';
   my $title  = $mpd->current->title  // 'undef';
   my $year   = $mpd->current->date   // 0;
-  my $playing = sprintf("^fg(#87d53c)%.50s ^fg()- ^fg(#03ab4a)%.50s^fg() (^fg(#3cd56c)%.30s^fg()) [^fg(#88eebd)%d^fg()]",
+  my $playing = sprintf("^fg(#87d53c)%.50s ^fg()- ^fg(#03ab4a)%.50s^fg() (^fg(#3cd56c)%.50s^fg()) [^fg(#88eebd)%d^fg()]",
     $artist, $title, $album, $year);
-  my $trunc = sprintf("%.300s", $playing);
+  my $trunc = sprintf("%s ^i($status)", $playing);
 
   return $trunc;
 }
 
-sub bg {
-  my $color = shift;
-  return "^bg($color)";
-}
-
-
 print "^bg()";
-print("^i($i_music)",mpd(), "  ", "^bg(#901D1D)", "^bg(4D4C4C)");
+print("^i($i_music)",mpd(), "  ", uptime(), "^bg(4D4C4C)");
 print "^bg()";
 
 # vim: set tw=99999:
