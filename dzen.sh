44e881ffb0f636db145b24c3f3a8e1b33211ef22
Magnus Woldrich <trapd00r@trapd00r.se>
2010-09-18 07:05:28 +0200

    some small fixes

--- dzen.sh
+++ dzen.sh
@@ -3,11 +3,12 @@
 FONT="-windows-montecarlo-medium-r-normal--11-110-72-72-c-60-microsoft-cp1252"
 while true; do 
   printf "^fg(#647474)"
-  date +%H:%M%p|perl -pe 's/(\d+):(\d+)((A|P)M)/^fg(#69996a)$1^fg():^fg(#69986b)$2^fg(#5c5c5c)$3/'
+  date +%H:%M%p |perl -pe 's/(\d+):(\d+)((A|P)M)/^fg(#69996a)$1^fg():^fg(#69986b)$2^fg(#5c5c5c)$3/'
   perl /home/scp1/devel/dzen-scripts/pdzen.pl
   #perl ./pdzen.pl
-  sleep 3
-done |dzen2 -e 'foo' -ta r -x 0 -y 0 -w 1366  -fg '#ffffff' -bg '#0e0e0e' \
-           -fn $FONT -h 15 -u -l 2 -h 17  
+  sleep 1
+done|dzen2 -fn $FONT -x 0 -y 0 -w 1366 -tw 1366  -ta r -u -l 2
+#done |dzen2 -e 'foo' -ta r -x 0 -y 0  -w 1366  -fg '#ffffff' -bg '#0e0e0e' \
+#           -fn $FONT -u -l 2
 
 # vim: tw=99999:
--- pdzen.pl
+++ pdzen.pl
@@ -1,8 +1,6 @@
 #!/usr/bin/perl
 use strict;
 
-my $circle  = "^co(2)";
-my $square  = "^r(2x2)";
 my $i_music = "/home/scp1/devel/dzen-scripts/bitmaps/musicS.xbm";
 my $i_mail  = "/home/scp1/devel/dzen-scripts/bitmaps/mail.xbm";
 my $i_bat   = "/home/scp1/devel/dzen-scripts/bitmaps/battery.xbm";
@@ -12,8 +10,6 @@ sub date {
   my $fdate = sprintf("%02s:%02s", $date[2], $date[1]);
   return $fdate;
 }
-
-#TODO
 
 sub uptime {
   chomp(my $up = `uptime`);
@@ -26,30 +22,102 @@ sub uptime {
   $_5 = load($_5);
   $_15 = load($_15);
 
-  return sprintf("$_1, $_5, $_15");
-
+  return sprintf("^fg(#5bde58)india^fg(): $_1^fg(#999999),^fg() $_5^fg(#999999),^fg() $_15");
 }
 
+sub shiva_uptime {
+  open(my $fh, 'ssh -p 19216 scp1@192.168.1.101 "uptime"|') or return("shiva: down");
+  chomp(my $up = <$fh>);
+  my ($_1,$_5,$_15) = $up =~ /average: (.+),(.+),(.+)$/;
+  $_1 =~ s/\s+//;
+  $_5 =~ s/\s+//;
+  $_15 =~ s/\s+//;
+
+  $_1 = load($_1);
+  $_5 = load($_5);
+  $_15 = load($_15);
+
+  return sprintf("^fg(#588dde)shiva^fg(): $_1^fg(#999999),^fg() $_5^fg(#999999),^fg() $_15");
+}
+
+sub dvdc_uptime {
+  open(my $fh, 'ssh -p 19216 scp1@192.168.1.100 "uptime"|') or return("dvdc: down");
+  chomp(my $up = <$fh>);
+  my ($_1,$_5,$_15) = $up =~ /average: (.+),(.+),(.+)$/;
+  $_1 =~ s/\s+//;
+  $_5 =~ s/\s+//;
+  $_15 =~ s/\s+//;
+
+  $_1 = load($_1);
+  $_5 = load($_5);
+  $_15 = load($_15);
+
+  return sprintf("^fg(#de5860)dvdc^fg(): $_1^fg(#999999),^fg() $_5^fg(#999999),^fg() $_15");
+}
+
+sub n900_uptime {
+  open(my $fh, 'ssh -p 19216 user@192.168.1.112 "uptime" 2> /dev/null|') or return("n900: down");
+  chomp(my $up = <$fh>);
+  my ($_1,$_5,$_15) = $up =~ /average: (.+),(.+),(.+)$/;
+  $_1 =~ s/\s+//;
+  $_5 =~ s/\s+//;
+  $_15 =~ s/\s+//;
+
+  $_1 = load($_1);
+  $_5 = load($_5);
+  $_15 = load($_15);
+
+  return sprintf("^fg(#f6f409)n900^fg(): $_1^fg(#999999),^fg() $_5^fg(#999999),^fg() $_15");
+}
+
+
 sub load {
-  my $load = shift;
-  if($load >= 0.30 and $load < 0.50) {
-    $load = "^fg(#03ab4a)$load^fg()";
+  chomp(my $load = shift);
+  if($load < 0.10) {
+    $load = "^fg(#09ff00)$load^fg()";
   }
-  elsif($load >= 0.75 and $load < 1.00) {
+  if($load >= 0.10 and $load <= 0.20) {
+    $load = "^fg(#28f809)$load^fg()";
+  }
+  elsif($load >= 0.21 and $load <= 0.30) {
     $load = "^fg(#f2b30e)$load^fg()";
   }
-  elsif($load >= 1.00) {
-    $load = "^fg(#ff0000)$load^fg()";
+  elsif($load >= 0.31 and $load <= 0.40) {
+    $load = "^fg(#f8d509)$load^fg()";
+  }
+  elsif($load >= 0.41 and $load <= 0.50) {
+    $load = "^fg(#f8b009)$load^fg()";
+  }
+  elsif($load >= 0.51 and $load <= 0.60) {
+    $load = "^fg(#f88e09)$load^fg()";
+  }
+  elsif($load >= 0.61 and $load <= 0.70) {
+    $load = "^fg(#f87209)$load^fg()";
+  }
+  elsif($load >= 0.71 and $load <= 0.80) {
+    $load = "^fg(#f85e09)$load^fg()";
+  }
+  elsif($load >= 0.81 and $load <= 0.90) {
+    $load = "^fg(#f84709)$load^fg()";
+  }
+  elsif($load >= 0.91 and $load <= 1.00) {
+    $load = "^fg(#f82409)$load^fg()";
+  }
+  elsif($load >= 1.01 and $load <= 1.10) {
+    $load = "^fg(#f81609)$load^fg()";
+  }
+  elsif($load >= 1.11 and $load <= 1.20) {
+    $load = "^fg(#ff0003)$load^fg()";
   }
   else {
-    $load = "^fg(#15f20e)$load^fg()";
+    $load = "^fg(#ff0000)$load^fg()";
   }
   return($load);
 }
 
 sub mail {
   open(my $fh, 'ssh -p 19216 scp1@192.168.1.101 "ls /mnt/Docs/Mail/inbox/new"|') or die($!);
-  my @new = <$fh>;
+  chomp(my @new = <$fh>);
   close($fh);
   my $count = scalar(@new);
 
@@ -58,17 +126,12 @@ sub mail {
 }
 
 sub battery {
-  open(my $fh, 'acpi|') or die($!);
+  open(my $fh, 'ibam --percentbattery|head -1|') or die($!);
   chomp(my $bat = <$fh>);
   close($fh);
 
-  $bat =~ s/: (.+), ([0-9]+)%,.+/$1 $2/;
-  my $status = $1;
-  my $percent = $2;
-  $status = ($status eq 'Full') ? 'Charging' : $status;
-  $percent = ($percent < 50) ? "^fg(#ff0000)$percent^fg()" : "^fg(#13c40f)$percent^fg()";
-
-  return("^i($i_bat) $status: $percent%");
+  $bat =~ s/Battery percentage:\s+(\d+).+/$1/;
+  return("^i($i_bat) $bat%");
 
 }
 
@@ -78,20 +141,29 @@ sub mpd {
   my $mpd = Audio::MPD->new(
     host => '192.168.1.101',
   );
-  my $status = "/home/scp1/devel/dzen-scripts/bitmaps/" . $mpd->status->state . '.xbm';
-  my $artist = $mpd->current->artist // 'undef';
-  my $album  = $mpd->current->album  // 'undef';
-  my $title  = $mpd->current->title  // 'undef';
-  my $year   = $mpd->current->date   // 0;
-  my $playing = sprintf("^fg(#87d53c)%.50s ^fg()- ^fg(#03ab4a)%.50s^fg() (^fg(#3cd56c)%.50s^fg()) [^fg(#88eebd)%d^fg()]",
-    $artist, $title, $album, $year);
-  my $trunc = sprintf("%s ^i($status)", $playing);
 
-  return $trunc;
+  my $current = $mpd->current;
+  if(!defined($current)) {
+    return(' MPD: ^fg(#ff0000)Stopped^fg()');
+  }
+
+  my $status = "/home/scp1/devel/dzen-scripts/bitmaps/" . $mpd->status->state . '.xbm';
+  my $artist = $current->artist // 'undef';
+
+  my $album  = $current->album  // 'undef';
+  my $title  = $current->title  // 'undef';
+  my $year   = $current->date   // 0;
+
+  my $pl = sprintf(" ^fg(#6be603)%.40s^fg(#999999),^fg() ^fg(#ff8700)%.40s", $title, $artist);
+
+  return($pl);
 }
 
 my $output = "^i($i_music)" . mpd()
   . "^fg(#484848) | ^fg()" . uptime()
+  . "^fg(#484848) | ^fg()" . shiva_uptime()
+  . "^fg(#484848) | ^fg()" . dvdc_uptime()
+#  . "^fg(#484848) | ^fg()" . n900_uptime()
   . "^fg(#484848) | ^fg()" . mail()
   . "^fg(#484848) | ^fg()" . battery()
   . "^fg(#484848) | ^fg()";
