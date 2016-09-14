2734b99f5fd96e58b685e2ff89461caff583f07a
Magnus Woldrich <magnus.woldrich@gmail.com>
2016-09-14 15:03:13 +0200

    dzen_elite.sh: add dzen script for elite host

---
+++ dzen_elite.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# vim:ft=zsh:
+
+    FONT='-windows-montecarlo-medium-r-normal--11-110-72-72-c-60-microsoft-cp1252'
+DZEN_OPT='-l 1 -dock -u -ta r -sa r -x 0 -y 0 -w 1600 -l 1 -bg "#121212"'
+
+while true; do
+  date '+%d %b ^fg(#ff003e)%H^fg() ^fg(#ff003e)%M^fg()' | perl -pe 's/\n/ | /g'
+  printf "^fg(#f05906)%d^fg() new mail | " $(ls $HOME/mail/new | wc -l)
+  printf "Bob says: ^fg(#56c7f6)"; sort -R ~/dev/bob/lyrics | head -1 | perl -pe 's/\n/ /'; printf "^fg()| "
+  acpi | perl -pe 's/Battery [0-9]: //' | perl -pe 's/([0-9]+%)/^fg(#0af43a)$1^fg()/'
+  sleep 60
+done | dzen2 -fn $FONT $DZEN_OPTIONS
+
